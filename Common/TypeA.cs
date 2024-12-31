f274f4b3b541af981d97eb529845aecb273901ee
HeBa-15 <hebamahmoudali1510@gmail.com>
2024-12-31 18:37:36 +0200

    Namespace Members - class , struct

--- Common/TypeA.cs
+++ Common/TypeA.cs
@@ -24,6 +24,34 @@ namespace Common
 
     public class TypeA
     {
+        //What You Can Write inside the Class?
+
+        // 1. Fields(Attributes | Member Variables)
+        // 2. Properties (Full, Automatic)
+        // 3. Indexers (Special Property)
+        // 4. Methods
+        // 5. Operators
+        // 6. Constructors
+        // 7. Destructors
+        // 8. Events
+        // 9. Static Members (Static Fields , Static Properties ,Static Methods , Static Constructor)
+        // 10. Nested Types (Classes , Structs,Interfaces , Enums and etc)
+
+
+
+        /// Allowed Access Modifiers inside the class? 
+        /// 1. private 
+        /// 2. private protected
+        /// 3. protected
+        /// 4. internal
+        /// 5. internal protected
+        /// 6. public
+
+       
+        /// Default Access Modifier inside the class and struct is 'private'
+        ///
+    
+    
     }
 
     class TypeC
@@ -32,5 +60,32 @@ namespace Common
         {
             TypeA typeA = new TypeA();
         }
+    }
+
+
+
+    public struct TypeZ
+    {
+        /// What You Can Write inside the struct?
+        /// 1. Fields(Attributes | Member Variables)
+        /// 2. Properties (Full, Automatic)
+        /// 3. Indexers (Special Property)
+        /// 4. Methods
+        /// 5. Operators
+        /// 6. Constructors
+        /// 7. Events
+        /// 8. Nested Types (Classes , Structs,Interfaces , Enums and etc)
+
+
+
+        // Allowed Access Modifiers inside the struct? 
+        // 1. private
+        // 2. internal
+        //3. public
+
+
+        /// Default Access Modifier inside the class and struct is 'private'
+        ///
+
     }
 }
